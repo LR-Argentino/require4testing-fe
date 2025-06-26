7ce08e22ce48af2cf78ea847e1eafc7b5d60f7f8
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-06-26 13:18:06 +0200

    add tailwindcss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Signin} from './signin/signin';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Signin],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
