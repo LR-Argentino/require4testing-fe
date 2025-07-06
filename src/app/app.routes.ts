09bb087db9aa7c5e8901eb9c6f77897828852b3f
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-07-06 09:59:57 +0200

    remove all routes and authentication guard from app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import {Routes} from '@angular/router';
-import {authGuard} from './guards/auth-guard';
 
-export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./components/dashboard/dashboard').then(m => m.Dashboard),
-    canActivate: [authGuard]
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./components/login/login').then(m => m.Login)
-  },
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-];
+export const routes: Routes = [];
--- src/app/models/LoginResponse.ts
+++
@@ -1,6 +0,0 @@
-export interface LoginResponse {
-  message: string;
-  roles: string[];
-  token: string;
-  username: string;
-}
